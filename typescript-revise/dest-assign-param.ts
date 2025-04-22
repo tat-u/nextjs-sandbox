11ed11285642d5f81e676a921389ef835fe3cbab
t_ueda <uetatsu1005@gmail.com>
2025-04-22 21:18:02 +0900

    Update dest-assign-param.ts

--- typescript-revise/dest-assign-param.ts
+++ typescript-revise/dest-assign-param.ts
@@ -7,6 +7,7 @@ type Contact = {
   email: string;
 };
 
+// 冗長かもしれないが、とりあえずこういうものがあるとする
 const contacts: Contact[] = [
   {
     name: "Jessie Milka",
@@ -25,16 +26,19 @@ const contacts: Contact[] = [
   },
 ];
 
+// 一つのオブジェクトとして受け取り、愚直に型注釈を書く
 function f1(contact: { name: string; phoneNumber: string; email: string }) {
   const { name, phoneNumber, email } = contact;
   console.log(name, phoneNumber, email);
 }
 
+// 一つのオブジェクトとして受け取り、typeで定義したものを用いて型注釈を書く
 function f2(contact: Contact) {
   const { name, phoneNumber, email } = contact;
   console.log(name, phoneNumber, email);
 }
 
+// 分割代入引数を用いる場合
 function f3({
   name,
   phoneNumber,
@@ -47,8 +51,42 @@ function f3({
   console.log(name, phoneNumber, email);
 }
 
+// 渡されるオブジェクトのプロパティのうち、一部だけ受け取ることも可能
+function f4({ name, phoneNumber }: { name: string; phoneNumber: string }) {
+  console.log(name, phoneNumber);
+}
+
+// 残余引数を使うことができる
+function f5({ name, ...rest }: { name: string }) {
+  console.log(name, rest);
+}
+
+// XXX: 受け取る側の引数名は、渡されるオブジェクトのプロパティ名と一致していないといけない
+// 「プロパティ a, b, c が無いため代入できない」と言われる
+function f6({ a, b, c }: { a: string; b: string; c: string }) {
+  console.log(a, b, c);
+}
+
+// 別名で受け取る場合
+// コロンの後に引数名が来て、プロパティ名に型注釈を行うのがポイント
+function f7({
+  name: a,
+  phoneNumber: b,
+  email: c,
+}: {
+  name: string;
+  phoneNumber: string;
+  email: string;
+}) {
+  console.log(a, b, c);
+}
+
 const contact = contacts[0];
 
 f1(contact);
 f2(contact);
 f3(contact);
+f4(contact);
+f5(contact);
+// f6(contact);
+f7(contact);
