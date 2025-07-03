45de1e534a005dc893deb5cfed2643b73d81625b
t_ueda <uetatsu1005@gmail.com>
2025-07-03 22:36:31 +0900

    Clean up project

--- background-tool/src/ui/components/CodeBlock/CodeBlock.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/ui/components/CodeBlock/CodeBlock.module.css";
-
-export function CodeBlock({ code }: { code: string }) {
-  return (
-    <pre className={styles.pre}>
-      <code>{code}</code>
-    </pre>
-  );
-}
