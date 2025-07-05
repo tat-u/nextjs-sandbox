e7596f873de1dda0701196bd7a9059e9beffcacb
t_ueda <uetatsu1005@gmail.com>
2025-07-06 01:21:03 +0900

    Update TextField.tsx

--- src/stories/molecules/TextField.tsx
+++ src/stories/molecules/TextField.tsx
@@ -2,19 +2,24 @@ import { useId } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
 import { Label } from "../atoms/Label";
 import { TextInput } from "../atoms/TextInput";
+import { FieldError } from "../atoms/FieldError";
 
 type Props = {
   label: string;
+  error: string | undefined;
   registerReturn?: UseFormRegisterReturn | undefined;
 };
 
-export function TextField({ label, registerReturn }: Props) {
+export function TextField({ label, registerReturn, error }: Props) {
   const id = useId();
 
   return (
     <div>
       <Label htmlFor={id}>{label}</Label>
       <TextInput id={id} {...registerReturn} />
+      <FieldError>{error}</FieldError>
     </div>
   );
 }
+
+// TODO: aria-describedby を使った a11y 対応
