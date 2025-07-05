f87ef91630174b7b2e917e3fbf30b2d6d6bed484
t_ueda <uetatsu1005@gmail.com>
2025-07-06 01:04:39 +0900

    Add component

---
+++ src/stories/atoms/FieldError.tsx
@@ -0,0 +1,7 @@
+import { HTMLAttributes, DetailedHTMLProps } from "react";
+
+type Props = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+export function FieldError(props: Props) {
+  return <div {...props} className="h-fit text-sm/[24px] text-red-600" />;
+}
