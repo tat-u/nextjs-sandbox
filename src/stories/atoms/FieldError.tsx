e17b832e61ad7f5ffbec2df50a3e49e1d9e24e56
t_ueda <uetatsu1005@gmail.com>
2025-07-06 01:20:27 +0900

    Fix height

--- src/stories/atoms/FieldError.tsx
+++ src/stories/atoms/FieldError.tsx
@@ -3,5 +3,5 @@ import { HTMLAttributes, DetailedHTMLProps } from "react";
 type Props = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 export function FieldError(props: Props) {
-  return <div {...props} className="h-fit text-sm/[24px] text-red-600" />;
+  return <div {...props} className="h-[24px] text-sm/[24px] text-red-600" />;
 }
