6e3f9ac30a2f3c3e7f9f235352f01aee94678b22
t_ueda <uetatsu1005@gmail.com>
2025-07-12 19:43:04 +0900

    Add components

---
+++ src/stories/molecules/PokemonTypeCard.tsx
@@ -0,0 +1,23 @@
+import { PokemonType, pokemonTypeColors } from "@/models/pokemonDefinitions";
+import { PokemonChip } from "../atoms/PokemonChip";
+
+type Props = { children: string; pokemonType: PokemonType; percentage: number };
+
+export function PokemonTypeCard({ children, pokemonType, percentage }: Props) {
+  return (
+    <div
+      className={`w-[144px] h-[32px] rounded-lg flex items-center shadow-sm overflow-hidden my-[4px]`}
+    >
+      <div
+        style={{
+          backgroundColor: pokemonTypeColors[pokemonType],
+        }}
+        className={`w-[8px] h-full`}
+      ></div>
+      <div className="flex w-full h-full flex-row items-center justify-between px-[8px]">
+        <div className="text-md/[14px]">{children}</div>
+        <PokemonChip percentage={percentage}>{`${percentage}%`}</PokemonChip>
+      </div>
+    </div>
+  );
+}
