ff02d233eb4fe8a3d8cc7008a98087b13b7c71d7
t_ueda <uetatsu1005@gmail.com>
2025-07-12 23:23:17 +0900

    Add themes

---
+++ src/models/themes.ts
@@ -0,0 +1,13 @@
+export const themes = {
+  bumblebee: { id: "bumblebee", name: "Bumblebee" },
+  halloween: { id: "halloween", name: "Halloween" },
+  lofi: { id: "lofi", name: "Lofi" },
+  valentine: { id: "valentine", name: "Valentine" },
+  caramellatte: { id: "caramellatte", name: "Caramel Latte" },
+  autumn: { id: "autumn", name: "Autumn" },
+  aqua: { id: "aqua", name: "Aqua" },
+  synthwave: { id: "synthwave", name: "Synthwave" },
+  night: { id: "night", name: "Night" },
+} as const;
+
+export type Theme = keyof typeof themes;
---
+++ src/stories/atoms/ThemeController.tsx
@@ -0,0 +1,41 @@
+import { themes } from "@/models/themes";
+
+/**
+ * This component requires daisyUI to be installed in your project.
+ */
+export function ThemeController() {
+  // WARN: なぜか daisyUI config で指定した prefix が反映されないため、一旦 "d-" prefix なしとする
+
+  return (
+    <div className="d-dropdown">
+      <div tabIndex={0} role="button" className="d-btn m-1">
+        Theme
+        <svg
+          width="12px"
+          height="12px"
+          className="inline-block h-2 w-2 fill-current opacity-60"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2048 2048"
+        >
+          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="d-dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl"
+      >
+        {Object.entries(themes).map(([key, theme]) => (
+          <li key={key}>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller w-full d-btn d-btn-sm d-btn-block d-btn-ghost justify-start"
+              aria-label={theme.name}
+              value={theme.id}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
