231038d83174559f696f5a70d3f3a68c0c7c48e6
t_ueda <uetatsu1005@gmail.com>
2025-07-12 12:58:25 +0900

    Update pokemonDefinitions.ts

--- src/models/pokemonDefinitions.ts
+++ src/models/pokemonDefinitions.ts
@@ -57,7 +57,7 @@ export type Vector18 = [
   number // 17
 ];
 
-export const effectivenessTable = {
+export const effectivenessTable: Record<PokemonType, Vector18> = {
   normal: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -2, 0, 0, -1, 0],
   fire: [0, -1, -1, 0, 1, 1, 0, 0, 0, 0, 0, 1, -1, 0, -1, 0, 1, 0],
   water: [0, 1, -1, 0, -1, 0, 0, 0, 1, 0, 0, 0, 1, 0, -1, 0, 0, 0],
@@ -76,9 +76,9 @@ export const effectivenessTable = {
   dark: [0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, -1, 0, -1],
   steel: [0, -1, -1, -1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -1, 1],
   fairy: [0, -1, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 1, 1, -1, 0],
-};
+} as const;
 
-export const defensivenessTable = {
+export const defensivenessTable: Record<PokemonType, Vector18> = {
   normal: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, -2, 0, 0, 0, 0],
   fire: [0, -1, 1, 0, -1, -1, 0, 0, 1, 0, 0, -1, 1, 0, 0, 0, -1, -1],
   water: [0, -1, -1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0],
@@ -97,4 +97,25 @@ export const defensivenessTable = {
   dark: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -2, 1, 0, -1, 0, -1, 0, 1],
   steel: [-1, 1, 0, 0, -1, -1, 1, -2, 1, -1, -1, -1, -1, 0, -1, 0, -1, -1],
   fairy: [0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, -1, 0, 0, -2, -1, 1, 0],
-};
+} as const;
+
+export const pokemonTypes = {
+  normal: { id: 0, name: { en: "Normal", ja: "ノーマル" } },
+  fire: { id: 1, name: { en: "Fire", ja: "ほのお" } },
+  water: { id: 2, name: { en: "Water", ja: "みず" } },
+  electric: { id: 3, name: { en: "Electric", ja: "でんき" } },
+  grass: { id: 4, name: { en: "Grass", ja: "くさ" } },
+  ice: { id: 5, name: { en: "Ice", ja: "こおり" } },
+  fighting: { id: 6, name: { en: "Fighting", ja: "かくとう" } },
+  poison: { id: 7, name: { en: "Poison", ja: "どく" } },
+  ground: { id: 8, name: { en: "Ground", ja: "じめん" } },
+  flying: { id: 9, name: { en: "Flying", ja: "ひこう" } },
+  psychic: { id: 10, name: { en: "Psychic", ja: "エスパー" } },
+  bug: { id: 11, name: { en: "Bug", ja: "むし" } },
+  rock: { id: 12, name: { en: "Rock", ja: "いわ" } },
+  ghost: { id: 13, name: { en: "Ghost", ja: "ゴースト" } },
+  dragon: { id: 14, name: { en: "Dragon", ja: "ドラゴン" } },
+  dark: { id: 15, name: { en: "Dark", ja: "あく" } },
+  steel: { id: 16, name: { en: "Steel", ja: "はがね" } },
+  fairy: { id: 17, name: { en: "Fairy", ja: "フェアリー" } },
+} as const;
