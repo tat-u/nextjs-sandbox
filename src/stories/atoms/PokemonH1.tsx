e87f3c49a2b9e0801aa754c08d6be42d50f5a763
t_ueda <uetatsu1005@gmail.com>
2025-07-12 19:46:26 +0900

    Adjust font size

--- src/stories/atoms/PokemonH1.tsx
+++ src/stories/atoms/PokemonH1.tsx
@@ -2,8 +2,8 @@ type Props = { children: string };
 
 export function PokemonH1({ children }: Props) {
   return (
-    <div className="my-[20px] border-t-2 border-b-2 border-stone-200 py-[16px]">
-      <h1 className="text-2xl/[24px] font-bold">{children}</h1>
+    <div className="my-[16px] border-t-2 border-b-2 border-stone-200 py-[12px]">
+      <h1 className="text-xl/[20px] font-bold">{children}</h1>
     </div>
   );
 }
--- src/stories/molecules/PokemonTypeCard.tsx
+++ src/stories/molecules/PokemonTypeCard.tsx
@@ -15,7 +15,7 @@ export function PokemonTypeCard({ children, pokemonType, percentage }: Props) {
         className={`w-[8px] h-full`}
       ></div>
       <div className="flex w-full h-full flex-row items-center justify-between px-[8px]">
-        <div className="text-md/[14px]">{children}</div>
+        <div className="text-sm/[14px]">{children}</div>
         <PokemonChip percentage={percentage}>{`${percentage}%`}</PokemonChip>
       </div>
     </div>
