b6dbb04dfd3d7affe6e9b3209e6fe8a592cee04a
t_ueda <uetatsu1005@gmail.com>
2025-07-13 09:32:41 +0900

    Complete basic feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PokemonTypeCard } from "@/stories/molecules/PokemonTypeCard";
 import { PokemonH1 } from "@/stories/atoms/PokemonH1";
 import { PokemonH2 } from "@/stories/atoms/PokemonH2";
@@ -5,23 +7,84 @@ import {
   generateRecommendedChart,
   getPokemonTypeName,
 } from "@/models/pokemonTypeChart";
-import { SupportedLanguages } from "@/models/pokemonDefinitions";
+import {
+  PokemonType,
+  pokemonTypes,
+  SupportedLanguages,
+} from "@/models/pokemonDefinitions";
+import { ThemeController } from "@/stories/atoms/ThemeController";
+import { PokemonDropdown } from "@/stories/atoms/PokemonDropdown";
+import { useState } from "react";
+import { typedObjectKeys, isPokemonType } from "@/models/pokemonUtils";
 
 export default function Home() {
+  const [playerTypeA, setPlayerTypeA] = useState<PokemonType>("normal");
+  const [playerTypeB, setPlayerTypeB] = useState<PokemonType | null>(null);
+  const [playerAttackType, setPlayerAttackType] =
+    useState<PokemonType>("normal");
+
+  const handlePlayerTypeAChange = (value: string) => {
+    if (isPokemonType(value)) {
+      setPlayerTypeA(value);
+    } else {
+      throw new Error();
+    }
+  };
+  const handlePlayerTypeBChange = (value: string) => {
+    if (isPokemonType(value)) {
+      setPlayerTypeB(value);
+    } else if (value === "none") {
+      setPlayerTypeB(null);
+    } else {
+      throw new Error();
+    }
+  };
+  const handlePlayerAttackTypeChange = (value: string) => {
+    if (isPokemonType(value)) {
+      setPlayerAttackType(value);
+    } else {
+      throw new Error();
+    }
+  };
+
   const {
     hasGoodDefenseAgainst,
     canInflictGoodDamageAgainst,
     hasPoorDefenseAgainst,
     canInflictPoorDamageAgainst,
-  } = generateRecommendedChart("rock", "dark", "fire");
+  } = generateRecommendedChart(playerTypeA, playerTypeB, playerAttackType);
   const lang = "ja" satisfies SupportedLanguages;
+
+  const options = typedObjectKeys(pokemonTypes).map((key) => ({
+    id: key,
+    name: getPokemonTypeName(key, lang),
+  }));
 
   return (
     <>
+      <ThemeController />
+
       <div>ここに選択するメニューが来る</div>
-      <div>タイプ1: いわ</div>
-      <div> タイプ2: あく</div>
-      <div> わざ: ほのお</div>
+      <div>タイプ1: {playerTypeA}</div>
+      <div>タイプ2: {playerTypeB}</div>
+      <div>わざ: {playerAttackType}</div>
+
+      <PokemonDropdown
+        name="player_type_a"
+        options={options}
+        handleChange={(value) => handlePlayerTypeAChange(value)}
+      />
+      <PokemonDropdown
+        name="player_type_b"
+        options={[{ id: "none", name: "なし" }, ...options]}
+        handleChange={(value) => handlePlayerTypeBChange(value)}
+      />
+      <PokemonDropdown
+        name="player_attack_type"
+        options={options}
+        handleChange={(value) => handlePlayerAttackTypeChange(value)}
+      />
+
       <PokemonH1>得意な相手ポケモン</PokemonH1>
       <PokemonH2>ダメージが入りやすい相手</PokemonH2>
       <div className="flex flex-wrap gap-2 justify-around ">
