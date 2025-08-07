2d0c786033b5140fd3a3788d35b29cca5ad72fa3
t_ueda <uetatsu1005@gmail.com>
2025-08-07 23:20:13 +0900

    Restructure directories under @/app

---
+++ src/app/example/form/page.tsx
@@ -0,0 +1,7 @@
+import { ReminderForm } from "@/stories/features/ReminderForm";
+
+// This is an example of React Hook Form and API Routes
+
+export default function Page() {
+  return <ReminderForm />;
+}
---
+++ src/app/example/use-state/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Counter } from "@/stories/molecules/Counter";
+
+export const metadata: Metadata = {
+  title: "Use State Example",
+  description: "An example of using useState in Next.js",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Counter />
+      <div>{children}</div>
+    </>
+  );
+}
